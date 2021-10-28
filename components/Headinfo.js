c8d0c7c7e08e774b5567dda9f5b49f0665538a11
jaehoLee <chosloo2835@gmail.com>
2021-10-28 21:19:15 +0900

    add sec-set

---
+++ components/Headinfo.js
@@ -0,0 +1,22 @@
+// Next.js 에서 제공하는 Head 컴포넌트
+import Head from 'next/head';
+
+const HeadInfo = ({ title, keyword, contents }) => {
+  return(
+    <div>
+      <Head>
+        <title>{ title }</title>
+        <meta keyword={ keyword } />
+        <meta contents={ contents } />
+      </Head>
+    </div>
+  );
+};
+
+HeadInfo.defaultProps = {
+  title: 'My Blog',
+  keyword: 'Blog made Next.js',
+  contents: 'Basic Next.js'
+}
+
+export default HeadInfo;
---
+++ pages/photos.js
@@ -0,0 +1,14 @@
+// Next 의 가장 큰 장점 중 하나, 라우팅이 상당히 쉬워짐. `pages/` 안에 파일을 작성할 때 마다 그 파일이름이 곧 url 주소가 됩니다.
+// (public 도 가능함.) (ex: `http://localhost:3000/photos`)
+import HeadInfo from "../components/Headinfo";
+
+const photos = () => {
+  return(
+    <div>
+      <HeadInfo title="My Blog Photos" />
+      <h1>My Photos</h1>
+    </div>
+  )
+}
+
+export default photos;
